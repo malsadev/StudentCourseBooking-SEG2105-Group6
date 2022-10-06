bbb0dd44bfe39b2df543dc6a0e83f17f5584cfb2
Mustafa Al-Sakkaf <malsa080@uottawa.ca>
2022-10-06 00:12:15 -0400

    User models and authenticator class

---
+++ app/src/main/java/com/example/studentcoursebooking_seg2105_group6/models/User.java
@@ -0,0 +1,58 @@
+package com.example.studentcoursebooking_seg2105_group6.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String username;
+    private String role;
+    private String password;
+    private String name;
+
+    //serializaation for cross-activity context transfer
+    public User() {
+
+    }
+
+    public User(String username, String password) {
+        this(username, password, "", "");
+    }
+
+    public User(String username, String password, String role, String name){
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
