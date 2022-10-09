ab473fad33ea579d64c3d6ee2da49d47e21ad274
emilielydr <emilielydr@gmail.com>
2022-10-09 13:05:48 -0400

    back buttons

--- app/src/main/java/com/example/studentcoursebooking_seg2105_group6/WelcomePage.java
+++ app/src/main/java/com/example/studentcoursebooking_seg2105_group6/WelcomePage.java
@@ -4,8 +4,11 @@ import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.studentcoursebooking_seg2105_group6.models.User;
@@ -21,5 +24,14 @@ public class WelcomePage extends AppCompatActivity {
         User signedUser = (User) getIntent().getSerializableExtra("signedUser");
         welcomeMessage.setText("Welcome " + signedUser.getRole() + " " + signedUser.getUsername());
 
+        Button welcomeBackBtn=(Button)findViewById(R.id.welcomeBackBtn);
+
+        welcomeBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomePage.this, MainActivity.class);
+                startActivity(intent);// should take to create account
+            }
+        });
     }
 }
