fa50ede45da036a97129106301cde5e837f40eb4
Myrtille Alesia Murangamirwa <59900597+MyrtilleAlesia@users.noreply.github.com>
2022-10-09 20:19:40 -0400

    defined Course class

--- app/src/main/java/com/example/studentcoursebooking_seg2105_group6/models/Course.java
+++ app/src/main/java/com/example/studentcoursebooking_seg2105_group6/models/Course.java
@@ -2,5 +2,20 @@ package com.example.studentcoursebooking_seg2105_group6.models;
 
 public class Course {
     String courseName;
+    int courseCode;
+
+    public Course(){
+
+    }
+
+    public Course(String course){
+        this.courseName = course;
+    }
+    public int getCode(int code){
+        this.courseCode = code;
+        return code;
+    }
+
+
 
 }
