c40bafa03e2e70fa375621674f50773a032aad07
Mustafa Al-Sakkaf <malsa080@uottawa.ca>
2022-10-10 15:20:36 -0400

    dynamic course list and course items list adapter

--- app/src/main/java/com/example/studentcoursebooking_seg2105_group6/models/Course.java
+++ app/src/main/java/com/example/studentcoursebooking_seg2105_group6/models/Course.java
@@ -1,21 +1,35 @@
 package com.example.studentcoursebooking_seg2105_group6.models;
 
-public class Course {
-    String courseName;
-    int courseCode;
+import java.io.Serializable;
+
+public class Course implements Serializable {
+    private String courseName;
+    private String courseCode;
+    private String courseDescription;
 
     public Course(){
 
     }
 
-    public Course(String course){
-        this.courseName = course;
-    }
-    public int getCode(int code){
-        this.courseCode = code;
-        return code;
+    public Course(String courseName,String courseCode){
+        this.courseName = courseName;
+        this.courseCode = courseCode;
     }
 
 
+    public String getCourseName() {
+        return courseName;
+    }
 
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 }
