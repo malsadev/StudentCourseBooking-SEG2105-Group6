ac9e6cdf0c62b458c44d80d4aef15eaa71a2c583
emilielydr <emilielydr@gmail.com>
2022-11-07 20:04:09 -0500

    DONT MERGE,pls try fix and help

--- app/src/main/java/com/example/studentcoursebooking_seg2105_group6/models/Course.java
+++ app/src/main/java/com/example/studentcoursebooking_seg2105_group6/models/Course.java
@@ -5,31 +5,10 @@ import java.util.ArrayList;
 
 
 public class Course implements Serializable {
-    private class Date implements Serializable{
-        private String day;
-        private String time;
-        public Date(String date, String time){
-            this.day=day;
-            this.time=time;
-        }
-        public String getDay(){
-            return day;
-        }
-        public String getTime(){
-            return time;
-        }
-        public void setDay(String day){
-            this.day=day;
-        }
-        public void setTime(String time){
-            this.time=time;
-        }
-    }
     private String courseName;
     private String courseCode;
     private String courseDescription = "";
     private ArrayList<Date> courseSchedule = new ArrayList<>();
-
 
     public Course() {
 
@@ -64,17 +43,11 @@ public class Course implements Serializable {
         courseDescription = description;
     }
 
+    public void addDate(Date date){
+        courseSchedule.add(date);
+    }
+
     public ArrayList<Date> getCourseSchedule(){
         return courseSchedule;
     }
-    public void addDate(String day, String time){
-        Date date=new Date(day, time);
-        courseSchedule.add(date);
-    }
-    // public String getCourseSchedule() {
-     //   return courseSchedule;
-    //}
-    //create class time object
-    //create class variable for course
-
 }
