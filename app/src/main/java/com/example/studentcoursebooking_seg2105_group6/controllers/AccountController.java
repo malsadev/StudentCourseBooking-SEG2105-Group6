0c8d1c1832ce0769cf2fe4148a788716921f644e
Mustafa Al-Sakkaf <malsa080@uottawa.ca>
2022-11-20 23:29:51 -0500

    added enrol button and implemented firestore api call method

--- app/src/main/java/com/example/studentcoursebooking_seg2105_group6/controllers/AccountController.java
+++ app/src/main/java/com/example/studentcoursebooking_seg2105_group6/controllers/AccountController.java
@@ -10,6 +10,7 @@ import com.example.studentcoursebooking_seg2105_group6.models.Course;
 import com.example.studentcoursebooking_seg2105_group6.models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -41,4 +42,29 @@ public class AccountController {
                 });
     }
 
+    public void addStudentToCourse(Course course, User user) {
+        db.collection("users")
+                .whereEqualTo("username", user.getUsername())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                db.collection("users").document(document.getId())
+                                        .update(
+                                                "courses", FieldValue.arrayUnion(course)
+                                        )
+                                        .addOnSuccessListener((doc) -> Log.d(TAG, "DocumentSnapshot successfully deleted!"))
+                                        .addOnFailureListener((e) -> Log.w(TAG, "Error deleting document", e));
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+
+    }
 }
