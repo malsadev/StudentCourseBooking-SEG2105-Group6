8d7aacf82faa331062078ffc019159457cd722fd
Myrtille Alesia Murangamirwa <59900597+MyrtilleAlesia@users.noreply.github.com>
2022-11-29 23:22:14 -0500

    Updated activity unenrolled courses list

--- app/src/main/java/com/example/studentcoursebooking_seg2105_group6/enrolledCoursesList.java
+++ app/src/main/java/com/example/studentcoursebooking_seg2105_group6/enrolledCoursesList.java
@@ -1,14 +1,51 @@
 package com.example.studentcoursebooking_seg2105_group6;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.studentcoursebooking_seg2105_group6.models.Course;
+import com.example.studentcoursebooking_seg2105_group6.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class enrolledCoursesList extends AppCompatActivity {
+   // FirebaseFirestore db;
+    User signedUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enrolled_courses_list);
+
+        signedUser.getCourseList();
+
+//        //List of all enrolled courses
+//        public ArrayList<String> enrolledCoursesList(Course course, User signedUser){
+//            ArrayList<String> coursesList = new ArrayList<>();
+//            db.collection("users")
+//                    .whereEqualTo("courses", course.getCourseCode())
+//                    .get()
+//                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                            if (task.isSuccessful()) {
+//                                coursesList.add(course.getCourseCode());
+//                            } else {
+//                                Log.d(TAG, "Error getting documents: ", task.getException());
+//                            }
+//                        }
+//                    });
+//            return coursesList;
+//        }
     }
 }
